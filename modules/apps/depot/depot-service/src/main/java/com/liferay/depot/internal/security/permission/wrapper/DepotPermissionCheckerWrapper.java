7be80b7a95fbf667462b8ad60fc68e393c271c6e
Adolfo Pérez <adolfo.perez@liferay.com>
2020-05-22 14:18:37 +0200

    LPS-110425 Only return true if owner of a depot

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/security/permission/wrapper/DepotPermissionCheckerWrapper.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/security/permission/wrapper/DepotPermissionCheckerWrapper.java
@@ -62,7 +62,7 @@ public class DepotPermissionCheckerWrapper extends PermissionCheckerWrapper {
 	public boolean hasPermission(
 		Group group, String name, long primKey, String actionId) {
 
-		if ((group != null) && isGroupOwner(group.getGroupId())) {
+		if (_isDepotGroupOwner(group)) {
 			return true;
 		}
 
@@ -75,7 +75,7 @@ public class DepotPermissionCheckerWrapper extends PermissionCheckerWrapper {
 	public boolean hasPermission(
 		Group group, String name, String primKey, String actionId) {
 
-		if ((group != null) && isGroupOwner(group.getGroupId())) {
+		if (_isDepotGroupOwner(group)) {
 			return true;
 		}
 
@@ -88,7 +88,7 @@ public class DepotPermissionCheckerWrapper extends PermissionCheckerWrapper {
 	public boolean hasPermission(
 		long groupId, String name, long primKey, String actionId) {
 
-		if (isGroupOwner(groupId)) {
+		if (_isDepotGroupOwner(_groupLocalService.fetchGroup(groupId))) {
 			return true;
 		}
 
@@ -101,7 +101,7 @@ public class DepotPermissionCheckerWrapper extends PermissionCheckerWrapper {
 	public boolean hasPermission(
 		long groupId, String name, String primKey, String actionId) {
 
-		if (isGroupOwner(groupId)) {
+		if (_isDepotGroupOwner(_groupLocalService.fetchGroup(groupId))) {
 			return true;
 		}
 
@@ -277,6 +277,16 @@ public class DepotPermissionCheckerWrapper extends PermissionCheckerWrapper {
 		if (_userGroupRoleLocalService.hasUserGroupRole(
 				getUserId(), group.getGroupId(),
 				DepotRolesConstants.ASSET_LIBRARY_CONTENT_REVIEWER, true)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isDepotGroupOwner(Group group) {
+		if ((group != null) && (group.getType() == GroupConstants.TYPE_DEPOT) &&
+			isGroupOwner(group.getGroupId())) {
 
 			return true;
 		}
