f7b5851144fb3654072be0a24ea867e88ea33bca
ricardocousolr <ricardo.couso@liferay.com>
2021-02-03 17:33:36 +0100

    LPS-107018 Adapt use of old method to new one

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -9121,7 +9121,7 @@ public class JournalArticleLocalServiceImpl
 						groupId,
 						classNameLocalService.getClassNameId(
 							JournalArticle.class),
-						resourcePrimKey, value);
+						resourcePrimKey, LocaleUtil.toLanguageId(key), value);
 
 				urlTitleMap.put(LocaleUtil.toLanguageId(key), urlTitle);
 			}
