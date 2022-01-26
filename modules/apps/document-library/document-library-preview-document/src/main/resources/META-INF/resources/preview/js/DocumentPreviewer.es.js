75b3d346f9cf8ae6bcfda1010e62435c01703419
Jorge Avalos <jorge.avalos@liferay.com>
2022-01-26 12:40:33 -0800

    LPS-146237 Added createImageURL method to create source URL correctly

--- modules/apps/document-library/document-library-preview-document/src/main/resources/META-INF/resources/preview/js/DocumentPreviewer.es.js
+++ modules/apps/document-library/document-library-preview-document/src/main/resources/META-INF/resources/preview/js/DocumentPreviewer.es.js
@@ -95,11 +95,19 @@ const DocumentPreviewer = ({baseImageURL, initialPage, totalPages}) => {
 		}, 100);
 	}
 
+	const createImageURL = (page) => {
+		const pos = baseImageURL.search('previewFileIndex=') + 17;
+		const imageURL =
+			baseImageURL.slice(0, pos) + page + baseImageURL.slice(pos);
+
+		return imageURL;
+	};
+
 	const loadPage = (page) => {
 		let pagePromise = loadedPages[page] && loadedPages[page].pagePromise;
 
 		if (!pagePromise) {
-			pagePromise = imagePromise(`${baseImageURL}${page}`).then(() => {
+			pagePromise = imagePromise(`${createImageURL(page)}`).then(() => {
 				loadedPages[page].loaded = true;
 			});
 
@@ -214,7 +222,7 @@ const DocumentPreviewer = ({baseImageURL, initialPage, totalPages}) => {
 						className={`preview-file-document ${
 							!expanded && 'preview-file-document-fit'
 						}`}
-						src={`${baseImageURL}${currentPage}`}
+						src={`${createImageURL(currentPage)}`}
 					/>
 				)}
 			</div>
