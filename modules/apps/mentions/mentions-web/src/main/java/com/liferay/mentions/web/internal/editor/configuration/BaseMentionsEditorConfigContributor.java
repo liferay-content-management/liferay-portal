659522d9b46e846b3cb39f18ab2ffd091beb4ef4
Kristof Loneck <kristof.loneck@liferay.com>
2022-04-11 16:35:08 +0200

    LPS-150871: Remove anchor unnecessary anchors from mentions

--- modules/apps/mentions/mentions-web/src/main/java/com/liferay/mentions/web/internal/editor/configuration/BaseMentionsEditorConfigContributor.java
+++ modules/apps/mentions/mentions-web/src/main/java/com/liferay/mentions/web/internal/editor/configuration/BaseMentionsEditorConfigContributor.java
@@ -21,6 +21,7 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -65,9 +66,10 @@ public class BaseMentionsEditorConfigContributor
 					).put(
 						"source",
 						() -> {
-							PortletURL portletURL = getPortletURL(
+							LiferayPortletURL portletURL = (LiferayPortletURL)getPortletURL(
 								themeDisplay, requestBackedPortletURLFactory);
 
+								portletURL.setAnchor(false);
 							return StringBundler.concat(
 								portletURL.toString(), "&",
 								PortalUtil.getPortletNamespace(
