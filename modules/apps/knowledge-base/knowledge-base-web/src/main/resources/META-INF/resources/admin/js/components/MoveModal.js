d279372c1cf8461d4c3ddb821e89e85b1856beef
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2023-06-15 16:06:20 +0200

    LPS-182766 Tree is not draggable in the move modal

--- modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/js/components/MoveModal.js
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/js/components/MoveModal.js
@@ -72,7 +72,6 @@ export default function MoveModal({itemToMoveParent, items: initialItems}) {
 				<ClayTreeView
 					defaultItems={items}
 					defaultSelectedKeys={new Set([itemToMoveParent])}
-					dragAndDrop
 					nestedKey="children"
 					onItemMove={handleItemMove}
 					showExpanderOnHover={false}
