9442763a3dde206c030f2230daa80f6e94ba5bbd
Roberto Díaz <roberto.diaz@liferay.com>
2023-07-14 09:23:55 +0200

    LPS-189055 Fix typo

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/AudioProcessorImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/AudioProcessorImpl.java
@@ -106,7 +106,7 @@ public class AudioProcessorImpl
 		CompanyLocalServiceUtil.forEachCompanyId(
 			companyId -> {
 				try {
-					String jobName = "generateVideoPreviews-".concat(
+					String jobName = "generateAudioPreviews-".concat(
 						PortalUUIDUtil.generate());
 
 					BackgroundTaskManagerUtil.addBackgroundTask(
