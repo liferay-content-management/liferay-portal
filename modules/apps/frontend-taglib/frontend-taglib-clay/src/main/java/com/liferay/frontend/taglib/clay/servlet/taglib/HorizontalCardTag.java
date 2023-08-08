bbdf4773f0b6710c24bbaa241ce55d42b8a55469
Jaime León <jaimeleonro@gmail.com>
2023-08-08 16:11:57 +0200

    LPS-170286 Refactor method to get title properly

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/HorizontalCardTag.java
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/HorizontalCardTag.java
@@ -63,20 +63,14 @@ public class HorizontalCardTag extends BaseCardTag {
 	}
 
 	public String getTitle() {
-		String title = _title;
-
 		HorizontalCard horizontalCard = getHorizontalCard();
 
 		if ((_title == null) && (horizontalCard != null)) {
-			title = horizontalCard.getTitle();
+			return horizontalCard.getTitle();
 		}
 
-		if (_isTranslated()) {
-			title = LanguageUtil.get(
-				TagResourceBundleUtil.getResourceBundle(pageContext), title);
-		}
-
-		return title;
+		return LanguageUtil.get(
+			TagResourceBundleUtil.getResourceBundle(pageContext), _title);
 	}
 
 	public void setHorizontalCard(HorizontalCard horizontalCard) {
