2cbebac012b0d6dbf66f0103562e575a4997465d
SamZiemer <samuel.ziemer@liferay.com>
2023-09-05 17:36:44 +0200

    LPS-190783 Add backend action to bulk copy dl entries

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/BulkCopyEntriesMVCActionCommand.java
@@ -0,0 +1,213 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.document.library.web.internal.portlet.action;
+
+import com.liferay.depot.group.provider.SiteConnectedGroupGroupProvider;
+import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileShortcut;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.service.DLFileShortcutLocalService;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sam Ziemer
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/bulk_copy_entries"
+	},
+	service = MVCActionCommand.class
+)
+public class BulkCopyEntriesMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		int errorCount = _copyEntries(actionRequest);
+
+		if (errorCount > 0) {
+			JSONPortletResponseUtil.writeJSON(
+				actionRequest, actionResponse,
+				JSONUtil.put(
+					"errorMessage",
+					_language.get(
+						themeDisplay.getLocale(),
+						"some-documents-folders-could-not-be-copied")));
+
+			hideDefaultSuccessMessage(actionRequest);
+		}
+		else {
+			JSONPortletResponseUtil.writeJSON(
+				actionRequest, actionResponse, _jsonFactory.createJSONObject());
+		}
+	}
+
+	private void _checkDestinationGroup(Group group) throws Exception {
+		if ((group != null) && group.isStaged() && !group.isStagingGroup()) {
+			throw new PortalException(
+				"cannot-copy-into-the-live-version-of-a-group");
+		}
+	}
+
+	private int _copyEntries(ActionRequest actionRequest) throws Exception {
+		int errorCount = 0;
+
+		long destinationFolderId = ParamUtil.getLong(
+			actionRequest, "destinationFolderId");
+
+		long destinationRepositoryId = ParamUtil.getLong(
+			actionRequest, "destinationRepositoryId");
+
+		Group group = _groupLocalService.fetchGroup(destinationRepositoryId);
+
+		_checkDestinationGroup(group);
+
+		long[] currentAndAncestorSiteAndDepotGroupIds =
+			_siteConnectedGroupGroupProvider.
+				getCurrentAndAncestorSiteAndDepotGroupIds(group.getGroupId());
+
+		long[] entryIds = ParamUtil.getLongValues(
+			actionRequest, "selectedEntries");
+
+		for (long entryId : entryIds) {
+			try {
+				DLFileEntry dlFileEntry =
+					_dlFileEntryLocalService.fetchDLFileEntry(entryId);
+
+				if (dlFileEntry != null) {
+					_dlAppService.copyFileEntry(
+						dlFileEntry.getFileEntryId(), destinationFolderId,
+						destinationRepositoryId,
+						dlFileEntry.getFileEntryTypeId(),
+						currentAndAncestorSiteAndDepotGroupIds,
+						ServiceContextFactory.getInstance(
+							DLFileEntry.class.getName(), actionRequest));
+
+					continue;
+				}
+
+				DLFileShortcut dlFileShortcut =
+					_dlFileShortcutLocalService.fetchDLFileShortcut(entryId);
+
+				if (dlFileShortcut != null) {
+					_dlAppService.copyFileShortcut(
+						dlFileShortcut.getFileShortcutId(), destinationFolderId,
+						destinationRepositoryId,
+						ServiceContextFactory.getInstance(
+							DLFileShortcut.class.getName(), actionRequest));
+
+					continue;
+				}
+
+				DLFolder dlFolder = _dlFolderLocalService.fetchDLFolder(
+					entryId);
+
+				if (dlFolder != null) {
+					_dlAppService.copyFolder(
+						dlFolder.getRepositoryId(), dlFolder.getFolderId(),
+						destinationRepositoryId, destinationFolderId,
+						_getFileEntryTypeIds(
+							group.getGroupId(), dlFolder.getFolderId()),
+						currentAndAncestorSiteAndDepotGroupIds,
+						ServiceContextFactory.getInstance(
+							DLFolder.class.getName(), actionRequest));
+				}
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException);
+				}
+
+				errorCount++;
+			}
+		}
+
+		return errorCount;
+	}
+
+	private Map<Long, Long> _getFileEntryTypeIds(long groupId, long folderId)
+		throws PortalException {
+
+		DLFolder folder = _dlFolderLocalService.getFolder(folderId);
+
+		long[] groupIds =
+			_siteConnectedGroupGroupProvider.
+				getCurrentAndAncestorSiteAndDepotGroupIds(groupId, true);
+
+		if (ArrayUtil.isEmpty(groupIds) ||
+			!ArrayUtil.contains(groupIds, folder.getGroupId())) {
+
+			return new HashMap<>();
+		}
+
+		return _dlFileEntryLocalService.getFileEntryTypeIds(
+			folder.getCompanyId(), folder.getGroupId(), folder.getTreePath());
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BulkCopyEntriesMVCActionCommand.class);
+
+	@Reference
+	private DLAppService _dlAppService;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private DLFileShortcutLocalService _dlFileShortcutLocalService;
+
+	@Reference
+	private DLFolderLocalService _dlFolderLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private SiteConnectedGroupGroupProvider _siteConnectedGroupGroupProvider;
+
+}
