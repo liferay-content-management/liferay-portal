6c3c0be6a819189da1919053847252984cb84c4f
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2023-11-24 12:48:52 +0100

    LPS-202559 html-top does not work on edit page mode

--- modules/apps/fragment/fragment-video-streaming/src/main/resources/META-INF/resources/js/VideoStreaming.js
+++ modules/apps/fragment/fragment-video-streaming/src/main/resources/META-INF/resources/js/VideoStreaming.js
@@ -42,8 +42,9 @@ export default function VideoStreaming({
 
 		const contentHeight = height || contentWidth * 0.5625;
 
-		content.firstElementChild.style.height = contentHeight + 'px';
-		content.firstElementChild.style.width = contentWidth + 'px';
+		const videoContainer = content.querySelector('.video-js');
+		videoContainer.style.height = contentHeight + 'px';
+		videoContainer.style.width = contentWidth + 'px';
 	}
 
 	// eslint-disable-next-line no-undef
