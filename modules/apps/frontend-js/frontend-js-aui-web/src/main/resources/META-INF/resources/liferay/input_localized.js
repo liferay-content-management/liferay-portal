840916568f4656531858fd338cb2ad863bb60a34
NemethNorbert <norbert.nemeth@liferay.com>
2024-03-12 15:44:54 +0100

    LPD-11253 we don't need to update the translationStatus on user input

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/input_localized.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/input_localized.js
@@ -674,8 +674,6 @@ AUI.add(
 					);
 
 					if (languageStatusNode) {
-						Liferay.fire('inputLocalized:updateTranslationStatus');
-
 						languageStatusNode.setHTML(
 							A.Lang.sub(instance.TRANSLATION_STATUS_TEMPLATE, {
 								languageId: languageId.replaceAll(/_/g, '-'),
