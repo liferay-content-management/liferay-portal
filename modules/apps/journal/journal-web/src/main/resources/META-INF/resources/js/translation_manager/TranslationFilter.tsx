7a51099a8050423a4acc45f0c1eac681ca8e7563
diegohu97 <diego.hu@liferay.com>
2024-05-13 13:22:09 +0200

    LPD-25634 We should compare with the total items when using "untranslated" filter

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/translation_manager/TranslationFilter.tsx
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/translation_manager/TranslationFilter.tsx
@@ -123,14 +123,14 @@ export default function TranslationFilter({
 						if (
 							(translationProgress?.translatedItems[
 								selectedLanguageId
-							] ?? 0) > 1
+							] ?? 0) < (translationProgress?.totalItems ?? 0)
 						) {
-							contentWrapper.hidden = true;
-							emptyPlaceholder.hidden = false;
-						}
-						else {
 							contentWrapper.hidden = false;
 							emptyPlaceholder.hidden = true;
+						}
+						else {
+							contentWrapper.hidden = true;
+							emptyPlaceholder.hidden = false;
 						}
 					}
 					else {
@@ -138,9 +138,9 @@ export default function TranslationFilter({
 						metadataWrapper.hidden = false;
 
 						if (
-							!translationProgress?.translatedItems[
+							(translationProgress?.translatedItems[
 								selectedLanguageId
-							]
+							] ?? 0) < (translationProgress?.totalItems ?? -1)
 						) {
 							contentWrapper.hidden = false;
 						}
