2579492453e72fee5204a597e66a09e2277f0733
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-05-15 13:14:29 +0200

    LPD-26178 New method to retrieve the termsfilter of the content dashboard filter

--- modules/apps/content-dashboard/content-dashboard-api/src/main/java/com/liferay/content/dashboard/item/filter/ContentDashboardItemFilter.java
+++ modules/apps/content-dashboard/content-dashboard-api/src/main/java/com/liferay/content/dashboard/item/filter/ContentDashboardItemFilter.java
@@ -6,6 +6,7 @@
 package com.liferay.content.dashboard.item.filter;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
 
 import java.util.List;
 import java.util.Locale;
@@ -28,6 +29,10 @@ public interface ContentDashboardItemFilter {
 	public String getParameterName();
 
 	public List<String> getParameterValues();
+
+	public default TermsFilter getTermsFilter() {
+		return null;
+	}
 
 	public Type getType();
 
