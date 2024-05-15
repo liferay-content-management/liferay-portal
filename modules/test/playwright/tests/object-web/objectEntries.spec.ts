e6bf3b1d34d9ef05bdfe38c0820a94d3ed5773eb
Igor Franca <igor.franca@liferay.com>
2024-05-15 10:57:53 -0300

    LPD-24100 Create a Test to Cover Object Entry on a Page Template

--- modules/test/playwright/tests/object-web/objectEntries.spec.ts
+++ modules/test/playwright/tests/object-web/objectEntries.spec.ts
@@ -10,7 +10,9 @@ import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
+import {getRandomInt} from '../../utils/getRandomInt';
 import getRandomString from '../../utils/getRandomString';
+import {journalPagesTest} from '../journal-web/fixtures/journalPagesTest';
 import {pageEditorPagesTest} from '../layout-content-page-editor-web/fixtures/pageEditorPagesTest';
 import getCollectionDefinition from '../layout-content-page-editor-web/utils/getCollectionDefinition';
 import getFragmentDefinition from '../layout-content-page-editor-web/utils/getFragmentDefinition';
@@ -23,6 +25,7 @@ export const test = mergeTests(
 	featureFlagsTest({
 		'LPS-178052': true,
 	}),
+	journalPagesTest,
 	loginTest(),
 	pageEditorPagesTest
 );
@@ -150,5 +153,100 @@ test.describe('Manage object entries through page templates', () => {
 		await apiHelpers.objectAdmin.deleteObjectDefinition(
 			objectDefinition.id
 		);
+	});
+
+	test('verify if the object entry title is displayed when selecting to preview an object entry on a page template', async ({
+		apiHelpers,
+		displayPageTemplatesPage,
+		page,
+		pageEditorPage,
+	}) => {
+		const randomObjectDefinition = `ObjectDefinition${getRandomInt()}`;
+		const randomObjectField = `objectField${getRandomInt()}`;
+
+		const objectDefinition =
+			await apiHelpers.objectAdmin.postObjectDefinition({
+				active: true,
+				label: {
+					en_US: randomObjectDefinition,
+				},
+				name: randomObjectDefinition,
+				objectFields: [
+					{
+						DBType: 'String',
+						businessType: 'Text',
+						externalReferenceCode: randomObjectField,
+						indexed: true,
+						indexedAsKeyword: false,
+						indexedLanguageId: '',
+						label: {en_US: randomObjectField},
+						listTypeDefinitionId: 0,
+						localized: false,
+						name: randomObjectField,
+						readOnly: 'false',
+						required: false,
+						state: false,
+						system: false,
+						type: 'String',
+					},
+				],
+				pluralLabel: {
+					en_US: randomObjectDefinition,
+				},
+				portlet: true,
+				scope: 'company',
+				status: {
+					code: 0,
+				},
+				titleObjectFieldName: randomObjectField,
+			});
+
+		const applicationName =
+			'c/' + objectDefinition.name.toLowerCase() + 's';
+
+		const textObjectEntry = {
+			[randomObjectField]: 'object entry',
+		};
+
+		await apiHelpers.objectEntry.postObjectEntry(
+			textObjectEntry,
+			applicationName
+		);
+
+		await displayPageTemplatesPage.goto();
+
+		const displayPageTemplateName = getRandomString();
+
+		await displayPageTemplatesPage.publishNewTemplate({
+			contentType: objectDefinition.label['en_US'],
+			name: displayPageTemplateName,
+		});
+
+		await page.getByTitle(displayPageTemplateName).click();
+
+		await pageEditorPage.dragFragmentToPageEditor('Heading');
+
+		await page.getByText('Heading Example', {exact: true}).click();
+
+		await page.getByLabel('Select element-text').click();
+
+		await pageEditorPage.setMappingConfiguration({
+			entity: randomObjectDefinition,
+			entry: 'object entry',
+			optionValue: `ObjectField_${randomObjectField}`,
+			source: 'Specific Content',
+		});
+
+		await expect(
+			page.getByTitle('Edit Text').filter({hasText: 'object entry'})
+		).toBeVisible();
+
+		// Clean up
+
+		await apiHelpers.objectAdmin.deleteObjectDefinition(
+			objectDefinition.id
+		);
+
+		await displayPageTemplatesPage.deleteAllDisplayPageTemplates();
 	});
 });
