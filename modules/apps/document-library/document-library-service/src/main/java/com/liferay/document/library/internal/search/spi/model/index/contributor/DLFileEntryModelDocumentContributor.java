3854becf84fd8de56a1a002eb79f38de26e452ff
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-05-20 10:22:52 +0200

    LPD-26182 Hide behind feature flag

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
@@ -30,6 +30,7 @@ import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.metadata.RawMetadataProcessor;
@@ -216,6 +217,10 @@ public class DLFileEntryModelDocumentContributor
 
 	private void _addFileEntryMetadataAttributes(
 		Document document, List<DLFileEntryMetadata> dlFileEntryMetadatas) {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-25680")) {
+			return;
+		}
 
 		for (DLFileEntryMetadata dlFileEntryMetadata : dlFileEntryMetadatas) {
 			try {
