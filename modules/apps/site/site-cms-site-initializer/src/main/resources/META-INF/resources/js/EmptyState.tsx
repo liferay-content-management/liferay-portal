aa7193851ac95a63e3b78c703b85edc7520610bb
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-01-24 11:28:42 +0100

    LPD-42855 Add dropdown button

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/EmptyState.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/EmptyState.tsx
@@ -3,29 +3,47 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton from '@clayui/button';
+import {ClayDropDownWithItems} from '@clayui/drop-down';
 import ClayEmptyState from '@clayui/empty-state';
 import React from 'react';
 
 import './../css/all_section.scss';
 
 interface IEmptyStateProps {
-    description?: string;
-    title?: string;
+	description?: string;
+	title?: string;
 }
 
 const EmptyState: React.FC<IEmptyStateProps> = ({
-    description = Liferay.Language.get(
-        'click-new-to-create-your-first-content'
-    ),
-    title = Liferay.Language.get('no-assets-yet'),
+	description = Liferay.Language.get(
+		'click-new-to-create-your-first-content'
+	),
+	title = Liferay.Language.get('no-assets-yet'),
 }) => {
-    return (
-        <ClayEmptyState
-            description={description}
-            imgSrc={`${Liferay.ThemeDisplay.getPathThemeImages()}/states/cms_empty_state.svg`}
-            title={title}
-        />
-    );
+	return (
+		<>
+			<ClayEmptyState
+				description={description}
+				imgSrc={`${Liferay.ThemeDisplay.getPathThemeImages()}/states/cms_empty_state.svg`}
+				title={title}
+			/>
+
+			<ClayDropDownWithItems
+				className="empty-state-new-dropdown"
+				items={[]}
+				trigger={
+					<ClayButton
+						aria-label={Liferay.Language.get('new')}
+						displayType="primary"
+					>
+						{Liferay.Language.get('new')}
+					</ClayButton>
+				}
+				triggerIcon="caret-bottom"
+			/>
+		</>
+	);
 };
 
 export default EmptyState;
