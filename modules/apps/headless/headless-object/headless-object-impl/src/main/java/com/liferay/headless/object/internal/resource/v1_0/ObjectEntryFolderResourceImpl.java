7cd8556629c18e1843f0b53b38d0593925ee8e6b
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2025-05-12 13:30:19 +0200

    LPD-54692 Use correct object entry folder ID

--- modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
+++ modules/apps/headless/headless-object/headless-object-impl/src/main/java/com/liferay/headless/object/internal/resource/v1_0/ObjectEntryFolderResourceImpl.java
@@ -399,7 +399,7 @@ public class ObjectEntryFolderResourceImpl
 
 		if (parentObjectEntryFolderId == null) {
 			parentObjectEntryFolderId =
-				persistedObjectEntryFolder.getObjectEntryFolderId();
+				persistedObjectEntryFolder.getParentObjectEntryFolderId();
 		}
 
 		return _toObjectEntryFolder(
