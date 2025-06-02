6d422e29092e4ec527de4d72a1673846ef2e9c21
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2025-06-02 16:31:17 +0200

    LPD-56913 Create base class to remove duplication

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/StructureBuilderFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/StructureBuilderFragmentRenderer.java
@@ -7,23 +7,16 @@ package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
 
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
-import com.liferay.frontend.taglib.react.servlet.taglib.ComponentTag;
 import com.liferay.object.admin.rest.resource.v1_0.ObjectDefinitionResource;
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.site.cms.site.initializer.internal.display.context.StructureBuilderDisplayContext;
-import com.liferay.taglib.servlet.PageContextFactoryUtil;
 import com.liferay.taglib.ui.SuccessTag;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
-
-import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -33,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = FragmentRenderer.class)
 public class StructureBuilderFragmentRenderer
-	extends BaseSectionFragmentRenderer {
+	extends BaseComponentSectionFragmentRenderer {
 
 	@Override
 	public String getCollectionKey() {
@@ -41,8 +34,8 @@ public class StructureBuilderFragmentRenderer
 	}
 
 	@Override
-	public String getLabel(Locale locale) {
-		return _language.get(locale, "structure-builder");
+	public String getLabelKey() {
+		return "structure-builder";
 	}
 
 	@Override
@@ -59,56 +52,37 @@ public class StructureBuilderFragmentRenderer
 
 			successTag.doTagAsString(httpServletRequest, httpServletResponse);
 
-			PrintWriter printWriter = httpServletResponse.getWriter();
-
-			printWriter.write("<div><span aria-hidden=\"true\" class=\"");
-			printWriter.write("loading-animation\"></span>");
-
-			ComponentTag componentTag = new ComponentTag();
-
-			componentTag.setModule(
-				"{StructureBuilder} from site-cms-site-initializer");
-			componentTag.setPageContext(
-				PageContextFactoryUtil.create(
-					httpServletRequest, httpServletResponse));
-
-			StructureBuilderDisplayContext structureBuilderDisplayContext =
-				new StructureBuilderDisplayContext(
-					httpServletRequest, _jsonFactory,
-					_objectDefinitionResourceFactory);
-
-			componentTag.setProps(structureBuilderDisplayContext.getProps());
-
-			componentTag.setServletContext(_servletContext);
-
-			componentTag.doStartTag();
-
-			componentTag.doEndTag();
-
-			printWriter.write("</div>");
+			super.render(
+				fragmentRendererContext, httpServletRequest,
+				httpServletResponse);
 		}
 		catch (Exception exception) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(exception);
-			}
+			ReflectionUtil.throwException(exception);
 		}
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		StructureBuilderFragmentRenderer.class);
+	@Override
+	protected String getModuleName() {
+		return "StructureBuilder";
+	}
+
+	@Override
+	protected Map<String, Object> getProps(
+		FragmentRendererContext fragmentRendererContext,
+		HttpServletRequest httpServletRequest) {
+
+		StructureBuilderDisplayContext structureBuilderDisplayContext =
+			new StructureBuilderDisplayContext(
+				httpServletRequest, _jsonFactory,
+				_objectDefinitionResourceFactory);
+
+		return structureBuilderDisplayContext.getProps();
+	}
 
 	@Reference
 	private JSONFactory _jsonFactory;
 
 	@Reference
-	private Language _language;
-
-	@Reference
 	private ObjectDefinitionResource.Factory _objectDefinitionResourceFactory;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.site.cms.site.initializer)"
-	)
-	private ServletContext _servletContext;
 
 }
