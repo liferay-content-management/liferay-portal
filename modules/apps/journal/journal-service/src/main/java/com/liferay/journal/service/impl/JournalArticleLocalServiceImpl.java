8221e9fd24621cc7ca380636b4a5cece31ef9ab9
Akhash R <akhash.ramprakash@liferay.com>
2025-06-10 11:13:32 +0530

    LPD-15151 Update the status later

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -4249,12 +4249,6 @@ public class JournalArticleLocalServiceImpl
 			throw new TrashEntryException();
 		}
 
-		int oldStatus = article.getStatus();
-
-		article = updateStatus(
-			userId, article.getId(), WorkflowConstants.STATUS_IN_TRASH,
-			new HashMap<>(), new ServiceContext());
-
 		List<JournalArticle> articleVersions =
 			journalArticlePersistence.findByG_A(
 				article.getGroupId(), article.getArticleId());
@@ -4275,6 +4269,12 @@ public class JournalArticleLocalServiceImpl
 		JournalArticleResource articleResource =
 			_journalArticleResourceLocalService.getArticleResource(
 				article.getResourcePrimKey());
+
+		int oldStatus = article.getStatus();
+
+		article = updateStatus(
+			userId, article.getId(), WorkflowConstants.STATUS_IN_TRASH,
+			new HashMap<>(), new ServiceContext());
 
 		TrashEntry trashEntry = _trashEntryLocalService.addTrashEntry(
 			userId, article.getGroupId(), JournalArticle.class.getName(),
@@ -4339,7 +4339,6 @@ public class JournalArticleLocalServiceImpl
 
 		return article;
 	}
-
 	/**
 	 * Moves the latest version of the web content article matching the group
 	 * and article ID to the recycle bin.
