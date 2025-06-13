146cd1b3f12cc0a1a5e21d7ba1f0e15c508fa3fe
Albertinin Mourato Santos <albertinin.mourato@liferay.com>
2025-06-13 17:41:44 +0200

    LPD-57472 Fix add space members page not loading after rebase

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/AddSpaceMembersFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/AddSpaceMembersFragmentRenderer.java
@@ -9,9 +9,7 @@ import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
-import com.liferay.frontend.taglib.react.servlet.taglib.ComponentTag;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -19,16 +17,10 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.site.cms.site.initializer.internal.util.ActionUtil;
-import com.liferay.taglib.servlet.PageContextFactoryUtil;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -38,7 +30,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = FragmentRenderer.class)
 public class AddSpaceMembersFragmentRenderer
-	extends BaseSectionFragmentRenderer {
+	extends BaseComponentSectionFragmentRenderer {
 
 	@Override
 	public String getCollectionKey() {
@@ -46,75 +38,51 @@ public class AddSpaceMembersFragmentRenderer
 	}
 
 	@Override
-	public String getLabel(Locale locale) {
-		return _language.get(locale, "add-members");
+	protected String getLabelKey() {
+		return "add-members";
 	}
 
 	@Override
-	public void render(
+	protected String getModuleName() {
+		return "AddSpaceMembers";
+	}
+
+	@Override
+	protected Map<String, Object> getProps(
 			FragmentRendererContext fragmentRendererContext,
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException {
+			HttpServletRequest httpServletRequest)
+		throws Exception {
 
-		try {
-			PrintWriter printWriter = httpServletResponse.getWriter();
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
-			printWriter.write("<div><span aria-hidden=\"true\" class=\"");
-			printWriter.write("loading-animation\"></span>");
+		long assetLibraryId = ParamUtil.getLong(
+			httpServletRequest, "assetLibraryId");
 
-			ComponentTag componentTag = new ComponentTag();
+		String assetLibraryName = StringPool.BLANK;
+		long creatorUserId = 0;
+		DepotEntry depotEntry = _depotEntryLocalService.fetchDepotEntry(
+			assetLibraryId);
 
-			componentTag.setModule(
-				"{AddSpaceMembers} from site-cms-site-initializer");
-			componentTag.setPageContext(
-				PageContextFactoryUtil.create(
-					httpServletRequest, httpServletResponse));
+		if (depotEntry != null) {
+			Group group = _groupLocalService.fetchGroup(
+				depotEntry.getGroupId());
 
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)httpServletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
-			long assetLibraryId = ParamUtil.getLong(
-				httpServletRequest, "assetLibraryId");
-
-			String assetLibraryName = StringPool.BLANK;
-			long creatorUserId = 0;
-			DepotEntry depotEntry = _depotEntryLocalService.fetchDepotEntry(
-				assetLibraryId);
-
-			if (depotEntry != null) {
-				Group group = _groupLocalService.fetchGroup(
-					depotEntry.getGroupId());
-
-				assetLibraryName = group.getDescriptiveName(
-					themeDisplay.getLocale());
-				creatorUserId = group.getCreatorUserId();
-			}
-
-			componentTag.setProps(
-				HashMapBuilder.<String, Object>put(
-					"assetLibraryCreatorUserId", creatorUserId
-				).put(
-					"assetLibraryId", assetLibraryId
-				).put(
-					"assetLibraryName", assetLibraryName
-				).put(
-					"baseAssetLibraryURL",
-					ActionUtil.getBaseSpaceURL(themeDisplay)
-				).build());
-
-			componentTag.setServletContext(_servletContext);
-
-			componentTag.doStartTag();
-
-			componentTag.doEndTag();
-
-			printWriter.write("</div>");
+			assetLibraryName = group.getDescriptiveName(
+				themeDisplay.getLocale());
+			creatorUserId = group.getCreatorUserId();
 		}
-		catch (Exception exception) {
-			throw new RuntimeException(exception);
-		}
+
+		return HashMapBuilder.<String, Object>put(
+			"assetLibraryCreatorUserId", creatorUserId
+		).put(
+			"assetLibraryId", assetLibraryId
+		).put(
+			"assetLibraryName", assetLibraryName
+		).put(
+			"baseAssetLibraryURL", ActionUtil.getBaseSpaceURL(themeDisplay)
+		).build();
 	}
 
 	@Reference
@@ -122,13 +90,5 @@ public class AddSpaceMembersFragmentRenderer
 
 	@Reference
 	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private Language _language;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.site.cms.site.initializer)"
-	)
-	private ServletContext _servletContext;
 
 }
