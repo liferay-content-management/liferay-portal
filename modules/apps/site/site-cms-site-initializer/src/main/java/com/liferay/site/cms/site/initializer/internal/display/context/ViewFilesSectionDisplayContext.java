c93aa664ab93914e8d0ac5534cf578d39a68a29b
Olivia Yu <olivia.yu@liferay.com>
2025-07-30 15:14:35 -0700

    LPD-49706 LPD-50637 site-cms-site-initializer: No need to preventDefault, remove old collaboratorByTypeURLs

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewFilesSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewFilesSectionDisplayContext.java
@@ -90,7 +90,7 @@ public class ViewFilesSectionDisplayContext
 		fdsActionDropdownItems.add(
 			3,
 			new FDSActionDropdownItem(
-				"#", "share", "share",
+				null, "share", "share",
 				LanguageUtil.get(httpServletRequest, "share"), "get", null,
 				"link"));
 
