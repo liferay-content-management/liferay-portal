f9972fad5fa81e3d501e48cc7b7f9b49de6d3125
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-08-21 10:35:46 +0200

    LPD-62919 Add link to cards. Used same mechanish than for tables, so this is based in SimpleActionLinkRenderer

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/utils/transformViewsItemProps.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/utils/transformViewsItemProps.ts
@@ -6,9 +6,32 @@
 import {IView} from '@liferay/frontend-data-set-web';
 
 import dateFormat from '../../../common/utils/dateFormat';
+import formatActionURL from '../../../common/utils/formatActionURL';
 
 const OBJECT_ENTRY_FOLDER_CLASSNAME =
 	'com.liferay.object.model.ObjectEntryFolder';
+
+const getHrefLink = (item: any, props: any) => {
+	const actionId = 'actionLink';
+	const {actions} = props;
+
+	if (!actions.length) {
+		return null;
+	}
+
+	const isFolder = item.entryClassName === OBJECT_ENTRY_FOLDER_CLASSNAME;
+	const resolvedActionId = isFolder ? `${actionId}Folder` : actionId;
+
+	const selectedAction = actions.find(
+		({data}: {data: any}) => data?.id === resolvedActionId
+	);
+
+	if (!selectedAction?.href) {
+		return null;
+	}
+
+	return formatActionURL(item, selectedAction.href);
+};
 
 const getThumbnailProps = (item: any) => {
 	if (item.entryClassName === OBJECT_ENTRY_FOLDER_CLASSNAME) {
@@ -36,6 +59,7 @@ export default function transformViewsItemProps(views: IView[]) {
 				return {
 					...props,
 					description: dateFormat(item.dateModified),
+					href: getHrefLink(item, props),
 					...getThumbnailProps(item),
 				};
 			};
