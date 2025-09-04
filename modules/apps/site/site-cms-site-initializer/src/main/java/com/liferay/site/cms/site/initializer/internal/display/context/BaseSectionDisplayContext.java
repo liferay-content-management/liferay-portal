99cb62a1a35df4735ae4143a4b81d5ff2ac7b430
Roberto Díaz <roberto.diaz@liferay.com>
2025-09-04 11:21:33 +0200

    LPD-64510 Fix typo

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
@@ -342,7 +342,7 @@ public abstract class BaseSectionDisplayContext {
 						httpServletRequest, TranslationPortletKeys.TRANSLATION,
 						ActionRequest.RENDER_PHASE)
 				).setMVCRenderCommandName(
-					"/translation/import_translation"
+					"/translation/export_translation"
 				).setParameter(
 					"className", "{entryClassName}"
 				).setParameter(
