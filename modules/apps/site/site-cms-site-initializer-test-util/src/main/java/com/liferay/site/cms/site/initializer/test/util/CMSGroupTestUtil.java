a655a519f878615cd18fb14572f6f7ac4c1bd644
Roberto Díaz <roberto.diaz@liferay.com>
2025-10-15 11:35:44 +0200

    LPD-68374 Add CMSGroupTestUtil to simplify CMS group initialization and update build dependencies

---
+++ modules/apps/site/site-cms-site-initializer-test-util/src/main/java/com/liferay/site/cms/site/initializer/test/util/CMSGroupTestUtil.java
@@ -0,0 +1,121 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.test.util;
+
+import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
+import com.liferay.batch.engine.unit.BatchEngineUnitReader;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.site.initializer.SiteInitializerRegistry;
+
+import java.io.File;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Roberto Díaz
+ */
+public class CMSGroupTestUtil {
+
+	public static Group getCMSGroup(
+			SiteInitializerRegistry siteInitializerRegistry, Class<?> clazz,
+			BatchEngineUnitProcessor batchEngineUnitProcessor,
+			BatchEngineUnitReader batchEngineUnitReader)
+		throws Exception {
+
+		Group group = GroupLocalServiceUtil.fetchGroup(
+			TestPropsValues.getCompanyId(), GroupConstants.CMS);
+
+		if (group != null) {
+			return group;
+		}
+
+		group = GroupTestUtil.addGroup(
+			TestPropsValues.getCompanyId(), TestPropsValues.getUserId(), 0,
+			GroupConstants.CMS);
+
+		String originalName = PrincipalThreadLocal.getName();
+
+		try {
+			PrincipalThreadLocal.setName(TestPropsValues.getUserId());
+
+			ServiceContextThreadLocal.pushServiceContext(
+				ServiceContextTestUtil.getServiceContext(group.getGroupId()));
+
+			try (SafeCloseable safeCloseable =
+					CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+						group.getCompanyId())) {
+
+				// These tests require the instance to be created with the
+				// feature flag LPD-17564 enabled. On CI, feature flags are
+				// enabled on demand for each test, but not during instance
+				// initialization. Until the feature flag LPD-17564 is removed,
+				// run the instance lifecycle initializer manually so that the
+				// role is created.
+
+				SiteInitializer siteInitializer =
+					siteInitializerRegistry.getSiteInitializer(
+						"com.liferay.site.initializer.cms");
+
+				siteInitializer.initialize(group.getGroupId());
+
+				Bundle testBundle = FrameworkUtil.getBundle(clazz);
+
+				BundleContext bundleContext = testBundle.getBundleContext();
+
+				for (Bundle bundle : bundleContext.getBundles()) {
+					if (Objects.equals(
+							bundle.getSymbolicName(),
+							"com.liferay.site.initializer.cms")) {
+
+						_deleteFile(bundle, "00.list.type.definition");
+						_deleteFile(bundle, "01.object.folder");
+						_deleteFile(bundle, "02.object.definition");
+
+						CompletableFuture<Void> completableFuture =
+							batchEngineUnitProcessor.processBatchEngineUnits(
+								batchEngineUnitReader.getBatchEngineUnits(
+									bundle));
+
+						completableFuture.join();
+					}
+				}
+			}
+		}
+		finally {
+			PrincipalThreadLocal.setName(originalName);
+
+			ServiceContextThreadLocal.popServiceContext();
+		}
+
+		return group;
+	}
+
+	private static void _deleteFile(Bundle bundle, String fileName) {
+		File file = bundle.getDataFile(
+			".com.liferay.site.initializer.cms.internal.batch." + fileName +
+				".batch.engine.data.json.0.processed");
+
+		if ((file != null) && file.exists()) {
+			file.delete();
+		}
+	}
+
+}
