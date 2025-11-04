32b85c2ff13360013ad0354d667a181c950b57d9
Roberto Díaz <roberto.diaz@liferay.com>
2025-11-04 12:28:57 +0100

    LPD-70535 Refactor `_getValue` logic for `ContentFieldValue` handling in `StructuredContentResourceImpl`

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -1156,6 +1156,20 @@ public class StructuredContentResourceImpl
 			sorts, this::_toStructuredContent);
 	}
 
+	private String _getValue(ContentFieldValue contentFieldValue) {
+		if (contentFieldValue.getData() != null) {
+			return contentFieldValue.getData();
+		}
+		else if (contentFieldValue.getDocument() != null) {
+			return String.valueOf(contentFieldValue.getDocument());
+		}
+		else if (contentFieldValue.getImage() != null) {
+			return String.valueOf(contentFieldValue.getImage());
+		}
+
+		return null;
+	}
+
 	private boolean _isNeverExpire(
 		StructuredContent structuredContent, JournalArticle journalArticle) {
 
@@ -1187,7 +1201,7 @@ public class StructuredContentResourceImpl
 				contentField.getContentFieldValue();
 
 			if ((contentFieldValue != null) &&
-				(contentFieldValue.getData() != null)) {
+				(_getValue(contentFieldValue) != null)) {
 
 				List<ContentFieldValue> contentFieldValues =
 					contentFieldValuesMap.computeIfAbsent(
@@ -1256,7 +1270,7 @@ public class StructuredContentResourceImpl
 					FieldConstants.getSerializable(
 						contextAcceptLanguage.getPreferredLocale(),
 						LocaleUtil.ROOT, field.getDataType(),
-						contentFieldValue.getData()));
+						_getValue(contentFieldValue)));
 			}
 
 			if (ListUtil.isNotEmpty(fieldValues)) {
