a0e3e05ddf4d7b60dfb7ea3bc254194492894a93
Akhash R <akhash.ramprakash@liferay.com>
2025-12-13 22:59:31 +0530

    LPD-73461 Add action items for tag export/import dropdown

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewTagsDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewTagsDisplayContext.java
@@ -5,13 +5,27 @@
 
 package com.liferay.site.cms.site.initializer.internal.display.context;
 
+import com.liferay.asset.tags.constants.AssetTagsAdminPortletKeys;
 import com.liferay.asset.util.AssetHelper;
-import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.exportimport.constants.ExportImportPortletKeys;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+
+import jakarta.portlet.PortletRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Map;
 
@@ -20,12 +34,30 @@ import java.util.Map;
  */
 public class ViewTagsDisplayContext {
 
-	public ViewTagsDisplayContext(ThemeDisplay themeDisplay) {
+	public ViewTagsDisplayContext(
+		GroupService groupService, HttpServletRequest httpServletRequest,
+		ThemeDisplay themeDisplay) {
+
+		_groupService = groupService;
+		_httpServletRequest = httpServletRequest;
 		_themeDisplay = themeDisplay;
 	}
 
-	public Map<String, Object> getReactData() throws PortalException {
+	public Map<String, Object> getReactData() throws Exception {
 		return HashMapBuilder.<String, Object>put(
+			"actionItems",
+			_putAll(
+				unsafeConsumer -> unsafeConsumer.accept(
+					JSONUtil.put(
+						"href",
+						_getControlPanelPortletURL(
+							AssetTagsAdminPortletKeys.ASSET_TAGS_ADMIN)
+					).put(
+						"label",
+						LanguageUtil.get(
+							_httpServletRequest, "export-import-tags")
+					)))
+		).put(
 			"cmsGroupId", _themeDisplay.getScopeGroupId()
 		).put(
 			"dataSetId", CMSSiteInitializerFDSNames.CATEGORIZATION_TAGS
@@ -56,6 +88,46 @@ public class ViewTagsDisplayContext {
 		).build();
 	}
 
+	private String _getControlPanelPortletURL(String portletId)
+		throws Exception {
+
+		return PortletURLBuilder.create(
+			PortalUtil.getControlPanelPortletURL(
+				_httpServletRequest,
+				_groupService.getGroup(_themeDisplay.getScopeGroupId()),
+				ExportImportPortletKeys.EXPORT_IMPORT, 0, 0,
+				PortletRequest.RENDER_PHASE)
+		).setMVCRenderCommandName(
+			"/export_import/export_import"
+		).setRedirect(
+			PortalUtil.getCurrentURL(_httpServletRequest)
+		).setPortletResource(
+			portletId
+		).setParameter(
+			"returnToFullPageURL", PortalUtil.getCurrentURL(_httpServletRequest)
+		).setWindowState(
+			LiferayWindowState.POP_UP
+		).buildString();
+	}
+
+	private JSONArray _putAll(
+			UnsafeConsumer<UnsafeConsumer<JSONObject, Exception>, Exception>
+				unsafeConsumer)
+		throws Exception {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		unsafeConsumer.accept(jsonArray::put);
+
+		if (jsonArray.length() == 0) {
+			return null;
+		}
+
+		return jsonArray;
+	}
+
+	private final GroupService _groupService;
+	private final HttpServletRequest _httpServletRequest;
 	private final ThemeDisplay _themeDisplay;
 
 }
