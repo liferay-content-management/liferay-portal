cef54d09808b82533c005c3a211ab336426b2677
Brian Chan <brian.chan@liferay.com>
2026-01-08 14:42:46 +0800

    LPD-74358 Consistency with _unlocalizedDDMStructure

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/StructuredContentResourceTest.java
@@ -200,7 +200,7 @@ public class StructuredContentResourceTest
 		_localizedComplexDDMStructure = _addComplexDDMStructure(true);
 		_localizedDDMStructure = _addDDMStructure(
 			testGroup, "test-localized-ddm-structure.json");
-		_unLocalizedComplexDDMStructure = _addComplexDDMStructure(true);
+		_unlocalizedComplexDDMStructure = _addComplexDDMStructure(true);
 		_unlocalizedDDMStructure = _addDDMStructure(
 			testGroup, "test-unlocalized-ddm-structure.json");
 	}
@@ -1321,7 +1321,7 @@ public class StructuredContentResourceTest
 			complexDDMStructure = _localizedComplexDDMStructure;
 		}
 		else {
-			complexDDMStructure = _unLocalizedComplexDDMStructure;
+			complexDDMStructure = _unlocalizedComplexDDMStructure;
 		}
 
 		Map<Locale, String> titleMap = HashMapBuilder.put(
@@ -3270,7 +3270,7 @@ public class StructuredContentResourceTest
 
 		Assert.assertTrue(
 			_equals(
-				_unLocalizedComplexDDMStructure, new String[] {"en-US"},
+				_unlocalizedComplexDDMStructure, new String[] {"en-US"},
 				structuredContent, putStructuredContent));
 
 		assertEquals(structuredContent, putStructuredContent);
@@ -3390,7 +3390,7 @@ public class StructuredContentResourceTest
 	@Inject
 	private RoleLocalService _roleLocalService;
 
-	private DDMStructure _unLocalizedComplexDDMStructure;
+	private DDMStructure _unlocalizedComplexDDMStructure;
 	private DDMStructure _unlocalizedDDMStructure;
 	private boolean _useDepotDDMStructureStructureId;
 
