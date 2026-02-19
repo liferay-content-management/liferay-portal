a4efc5268cab5f9f9c6edd1063ff12f1d37268f4
Manuel Rives <manuel.rives@liferay.com>
2026-02-19 09:29:05 +0100

    LPD-73608 Add test.

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -1175,6 +1175,53 @@ public class ObjectDefinitionLocalServiceTest {
 
 	@FeatureFlag("LPD-17564")
 	@Test
+	public void testAddObjectDefinitionWithSeveralAcceptedGroups()
+		throws Exception {
+
+		DepotEntry depotEntry1 = _depotEntryLocalService.addDepotEntry(
+			RandomTestUtil.randomLocaleStringMap(), Collections.emptyMap(),
+			DepotConstants.TYPE_ASSET_LIBRARY,
+			ServiceContextTestUtil.getServiceContext());
+		DepotEntry depotEntry2 = _depotEntryLocalService.addDepotEntry(
+			RandomTestUtil.randomLocaleStringMap(), Collections.emptyMap(),
+			DepotConstants.TYPE_ASSET_LIBRARY,
+			ServiceContextTestUtil.getServiceContext());
+		DepotEntry depotEntry3 = _depotEntryLocalService.addDepotEntry(
+			RandomTestUtil.randomLocaleStringMap(), Collections.emptyMap(),
+			DepotConstants.TYPE_ASSET_LIBRARY,
+			ServiceContextTestUtil.getServiceContext());
+
+		String objectName = ObjectDefinitionTestUtil.getRandomName();
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(depotEntry1.getGroupId());
+		sb.append(StringPool.COMMA);
+		sb.append(depotEntry2.getGroupId());
+		sb.append(StringPool.COMMA);
+		sb.append(depotEntry3.getGroupId());
+
+		String acceptedIDs = sb.toString();
+
+		ObjectDefinition objectDefinition = _publishCustomObjectDefinition(
+			objectName, ObjectDefinitionConstants.SCOPE_DEPOT,
+			Collections.singletonList(
+				new ObjectDefinitionSettingBuilder(
+				).name(
+					ObjectDefinitionSettingConstants.NAME_ACCEPTED_GROUP_IDS
+				).value(
+					acceptedIDs
+				).build()));
+
+		_assertObjectDefinitionSettingsValues(
+			objectDefinition.getObjectDefinitionSettings(),
+			Collections.singletonMap(
+				ObjectDefinitionSettingConstants.NAME_ACCEPTED_GROUP_IDS,
+				acceptedIDs));
+	}
+
+	@FeatureFlag("LPD-17564")
+	@Test
 	public void testAddObjectDefinitionWithWorkflowDefinitionLinks()
 		throws Exception {
 
