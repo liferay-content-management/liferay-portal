d59518514b8974745f3208070cff0556a5d019da
Kayleen Lim <kayleen.lim@liferay.com>
2026-02-20 12:18:59 -0800

    LPD-79076 Add test for trash entry not visible to other sites

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -174,6 +174,7 @@ import {config as smokeConfig} from './tests/smoke/main/config';
 import {config as stagingConfig} from './tests/staging-configuration-web/main/config';
 import {config as stylebookWebConfig} from './tests/style-book-web/main/config';
 import {config as templateWebConfig} from './tests/template-web/main/config';
+import {config as trashWebConfig} from './tests/trash-web/main/config';
 import {config as usersAdminWebEmailConfig} from './tests/users-admin-web/email/config';
 import {config as usersAdminWebConfig} from './tests/users-admin-web/main/config';
 import {config as usersAdminWebPermissionsConfig} from './tests/users-admin-web/permissions/config';
@@ -367,6 +368,7 @@ export default defineConfig({
 		stagingConfig,
 		stylebookWebConfig,
 		templateWebConfig,
+		trashWebConfig,
 		usersAdminWebConfig,
 		usersAdminWebEmailConfig,
 		usersAdminWebPermissionsConfig,
