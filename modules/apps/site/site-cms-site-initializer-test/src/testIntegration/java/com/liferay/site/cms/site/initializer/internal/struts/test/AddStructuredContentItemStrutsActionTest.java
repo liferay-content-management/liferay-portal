413ffa93e55e863086a70b875ae68ec15a58252e
Roberto Díaz <roberto.diaz@liferay.com>
2026-04-01 10:27:01 +0200

    LPD-84066 Update test

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/struts/test/AddStructuredContentItemStrutsActionTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/struts/test/AddStructuredContentItemStrutsActionTest.java
@@ -52,6 +52,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -297,7 +298,8 @@ public class AddStructuredContentItemStrutsActionTest {
 
 		ObjectEntry objectEntry = objectEntries.get(objectEntries.size() - 1);
 
-		Assert.assertTrue(objectEntry.isDraft());
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_EMPTY_DRAFT, objectEntry.getStatus());
 
 		Assert.assertEquals(
 			StringBundler.concat(
